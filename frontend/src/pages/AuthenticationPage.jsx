f9545f93e71af2346645739a40ab925dc7507a13
Royden Dsouza <81296538+roydendsouza31@users.noreply.github.com>
2024-05-01 14:35:33 +0530

    fixed authentication code

--- frontend/src/pages/AuthenticationPage.jsx
+++ frontend/src/pages/AuthenticationPage.jsx
@@ -1,30 +1,11 @@
-import * as React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  Grid,
-  Box,
-  Typography,
-  Paper,
-  Checkbox,
-  FormControlLabel,
-  TextField,
-  CssBaseline,
-  IconButton,
-  InputAdornment,
-  CircularProgress,
-} from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { LightPurpleButton } from "../utils/buttonStyles";
 import { authUser } from "../redux/userHandle";
-import styled from "styled-components";
+import "./AuthenticationPage.css";
 import Popup from "../components/Popup";
 
 const AuthenticationPage = ({ mode, role }) => {
-  const bgpic =
-    "https://images.pexels.com/photos/1121097/pexels-photo-1121097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -32,65 +13,38 @@ const AuthenticationPage = ({ mode, role }) => {
     (state) => state.user
   );
 
-  const [toggle, setToggle] = useState(false);
+  const [toggle] = useState(false);
   const [loader, setLoader] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
   const [message, setMessage] = useState("");
 
-  const [emailError, setEmailError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [userNameError, setUserNameError] = useState(false);
-  const [shopNameError, setShopNameError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [shopName, setShopName] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-
-    console.log(email, password);
     if (!email || !password) {
-      if (!email) setEmailError(true);
-      if (!password) setPasswordError(true);
       return;
     }
 
     if (mode === "Register") {
-      const name = event.target.userName.value;
-
-      if (!name) {
-        if (!name) setUserNameError(true);
+      if (!name || (role === "Seller" && !shopName)) {
         return;
       }
 
-      if (role === "Seller") {
-        const shopName = event.target.shopName.value;
-
-        if (!shopName) {
-          if (!shopName) setShopNameError(true);
-          return;
-        }
-
-        const sellerFields = { name, email, password, role, shopName };
-        dispatch(authUser(sellerFields, role, mode));
-      } else {
-        const customerFields = { name, email, password, role };
-
-        dispatch(authUser(customerFields, role, mode));
-      }
+      const fields =
+        role === "Seller"
+          ? { name, email, password, role, shopName }
+          : { name, email, password, role };
+      dispatch(authUser(fields, role, mode));
     } else if (mode === "Login") {
       const fields = { email, password };
       dispatch(authUser(fields, role, mode));
     }
     setLoader(true);
-  };
-
-  const handleInputChange = (event) => {
-    const { name } = event.target;
-    if (name === "email") setEmailError(false);
-    if (name === "password") setPasswordError(false);
-    if (name === "userName") setUserNameError(false);
-    if (name === "shopName") setShopNameError(false);
   };
 
   useEffect(() => {
@@ -109,166 +63,76 @@ const AuthenticationPage = ({ mode, role }) => {
 
   return (
     <>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <StyledTypography>
-              {role} {mode}
-            </StyledTypography>
+      <div class="container">
+        <form onSubmit={handleSubmit}>
+          <h4 class="heading">
+            {role} {mode}
+          </h4>
 
-            {role === "Seller" && mode === "Register" && (
-              <Typography variant="h7">
-                Create your own shop by registering as an seller.
-                <br />
-                You will be able to add products and sell them.
-              </Typography>
-            )}
+          {role === "Seller" && mode === "Register" && (
+            <p class="paragraph">
+              Create your own shop by registering as an seller.
+              <br />
+              You will be able to add products and sell them.
+            </p>
+          )}
 
-            {role === "Customer" && mode === "Register" && (
-              <Typography variant="h7">
-                Register now to explore and buy products.
-              </Typography>
-            )}
+          {role === "Customer" && mode === "Register" && (
+            <p class="paragraph">Register now to explore and buy products.</p>
+          )}
 
-            {mode === "Login" && (
-              <Typography variant="h7">
-                Welcome back! Please enter your details
-              </Typography>
-            )}
+          {mode === "Login" && (
+            <p class="paragraph">Welcome back! Please enter your details</p>
+          )}
 
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 2 }}
+          {mode === "Register" && (
+            <input
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
+
+          {mode === "Register" && role === "Seller" && (
+            <input
+              type="text"
+              placeholder="Create your shop name"
+              value={shopName}
+              onChange={(e) => setShopName(e.target.value)}
+            />
+          )}
+
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type={toggle ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label class="checkbox-label">
+            <input type="checkbox" />
+            Remember me
+          </label>
+          <button type="submit">{loader ? "Loading..." : mode}</button>
+          <div class="grid">
+            {mode === "Register"
+              ? "Already have an account?"
+              : "Don't have an account?"}
+            <Link
+              to={mode === "Register" ? `/${role}login` : `/${role}register`}
             >
-              {mode === "Register" && (
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="userName"
-                  label="Enter your name"
-                  name="userName"
-                  autoComplete="name"
-                  autoFocus
-                  variant="standard"
-                  error={userNameError}
-                  helperText={userNameError && "Name is required"}
-                  onChange={handleInputChange}
-                />
-              )}
-              {mode === "Register" && role === "Seller" && (
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="shopName"
-                  label="Create your shop name"
-                  name="shopName"
-                  autoComplete="off"
-                  variant="standard"
-                  error={shopNameError}
-                  helperText={shopNameError && "Shop name is required"}
-                  onChange={handleInputChange}
-                />
-              )}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Enter your email"
-                name="email"
-                autoComplete="email"
-                variant="standard"
-                error={emailError}
-                helperText={emailError && "Email is required"}
-                onChange={handleInputChange}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type={toggle ? "text" : "password"}
-                id="password"
-                autoComplete="current-password"
-                variant="standard"
-                error={passwordError}
-                helperText={passwordError && "Password is required"}
-                onChange={handleInputChange}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={() => setToggle(!toggle)}>
-                        {toggle ? <Visibility /> : <VisibilityOff />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Grid
-                container
-                sx={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-              </Grid>
-              <LightPurpleButton
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                {loader ? <CircularProgress size={24} color="inherit" /> : mode}
-              </LightPurpleButton>
-              <Grid container>
-                <Grid>
-                  {mode === "Register"
-                    ? "Already have an account?"
-                    : "Don't have an account?"}
-                </Grid>
-                <Grid item sx={{ ml: 2 }}>
-                  {mode === "Register" ? (
-                    <StyledLink to={`/${role}login`}>Log in</StyledLink>
-                  ) : (
-                    <StyledLink to={`/${role}register`}>Sign up</StyledLink>
-                  )}
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: `url(${bgpic})`,
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-      </Grid>
+              {mode === "Register" ? "Log in" : "Sign up"}
+            </Link>
+          </div>
+        </form>
+      </div>
       <Popup
         message={message}
         setShowPopup={setShowPopup}
@@ -279,19 +143,3 @@ const AuthenticationPage = ({ mode, role }) => {
 };
 
 export default AuthenticationPage;
-
-const StyledLink = styled(Link)`
-  margin-top: 9px;
-  text-decoration: none;
-  color: #7f56da;
-`;
-
-const StyledTypography = styled.h4`
-  margin: 0;
-  font-weight: 400;
-  font-size: 2.125rem;
-  line-height: 1.235;
-  letter-spacing: 0.00735em;
-  color: #2c2143;
-  margin-bottom: 16px;
-`;
